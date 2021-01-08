717b65daa44a15e7fff4eaf9abe06dc5d29bcb54
Tiia Kansa <como2e2@gmail.com>
2021-01-07 19:21:08 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularLab1';
+  title = 'TODO List';
 }
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,56 @@
+import { i18nMetaToDocStmt } from '@angular/compiler/src/render3/view/i18n/meta';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../interfaces/todo'
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+
+  todos: Todo[] = [
+    { task: "Make viral TikTok", completed: false },
+    { task: "Finish Todo App", completed: false },
+    { task: "Feed Fluffy", completed: true },
+    { task: "Change sweatsuit", completed: false }
+  ]
+
+  newTodo: string;
+  filterText: string;
+  filterTodos: Todo[];
+  todo: Todo;
+
+  addTodo = function(): void {
+    this.todo = {
+      task: this.newTodo,
+      completed: false
+    }
+    this.todos.push(this.todo);
+  }
+
+  completeTodo = function(todo): void {
+    let t : Todo = this.todos.find(t => t.task === todo.task);
+    t.completed = true;
+  }
+
+  filter = function():void {
+  
+      let filteredTodos = this.todos.filter(t => t.task.includes(this.filterText));
+      this.todos = filteredTodos;
+    
+  }
+
+  removeTodo = function(todo): void{
+    let index: number = this.todos.findIndex(t => t.task === todo.task)
+    this.todos.splice(index, 1);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
