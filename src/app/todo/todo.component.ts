e8f3a2a8faf54586b90a754a0d9e10ffada82ba1
Tiia Kansa <como2e2@gmail.com>
2021-01-21 10:25:57 -0500

    cleaned up code

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,56 +1,47 @@
 import { i18nMetaToDocStmt } from '@angular/compiler/src/render3/view/i18n/meta';
 import { Component, OnInit } from '@angular/core';
-import { Todo } from '../interfaces/todo'
+import { Todo } from '../interfaces/todo';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent implements OnInit {
-
   todos: Todo[] = [
-    { task: "Make viral TikTok", completed: false },
-    { task: "Finish Todo App", completed: false },
-    { task: "Feed Fluffy", completed: true },
-    { task: "Change sweatsuit", completed: false }
-  ]
+    { task: 'Make viral TikTok', completed: false },
+    { task: 'Finish Todo App', completed: false },
+    { task: 'Feed Fluffy', completed: true },
+    { task: 'Change sweatsuit', completed: false },
+  ];
 
   newTodo: string;
-  filterText: string;
-  filterTodos: Todo[];
+  filterText: string = '';
   todo: Todo;
 
-  addTodo = function(): void {
+  addTodo = function (): void {
     this.todo = {
       task: this.newTodo,
-      completed: false
-    }
+      completed: false,
+    };
     this.todos.push(this.todo);
-  }
+    this.newTodo = '';
+  };
 
-  completeTodo = function(todo): void {
-    let t : Todo = this.todos.find(t => t.task === todo.task);
-    t.completed = true;
-  }
+  completeTodo = function (todo: Todo): void {
+    todo.completed = true;
+  };
 
-  filter = function():void {
-  
-      let filteredTodos = this.todos.filter(t => t.task.includes(this.filterText));
-      this.todos = filteredTodos;
-    
-  }
+  filter = function (): Todo[] {
+    return this.todos.filter((t) => t.task.includes(this.filterText));
+  };
 
-  removeTodo = function(todo): void{
-    let index: number = this.todos.findIndex(t => t.task === todo.task)
+  removeTodo = function (todo: Todo): void {
+    let index: number = this.todos.findIndex(t => t.task === todo.task);
     this.todos.splice(index, 1);
-  }
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
-
-
